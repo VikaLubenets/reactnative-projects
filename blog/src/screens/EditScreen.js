0b8d9540c56d0be5dde72df94f2b6eacb41b2b01
Vika Lubenets <lubenetcvika@gmail.com>
2023-12-24 18:19:04 +0400

    init: init a new blog app

---
+++ blog/src/screens/EditScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>This is an edit screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
---
+++ blog/src/screens/ShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const ShowScreen = () => {
+  return (
+    <View>
+      <Text>This is a show screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default ShowScreen;
